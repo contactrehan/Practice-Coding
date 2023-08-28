3f8adc8d9d6fe176ca675dbe6392c88813419128
contactrehan <contactrehan2209@gmail.com>
2023-08-29 04:20:54 +0500

    28Aug-23

---
+++ 28Aug-23/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// For Each Loop
+let arr3 = [2, 3, 4, 5, 7];
+arr3.forEach = (function (k) {
+    console.log(k);
+});
--- 28Aug-23/index.ts
+++ 28Aug-23/index.ts
@@ -1,1 +1,11 @@
-console.log(`Practice Starts from Today`)
+// For Each Loop
+var Names=["Rehan","Ali","Usama","Bilawal"]
+Names.forEach((value)=>{
+    console.log(value); 
+})
+//consoling the square of each element in Array
+var Num=[2,4,6,8,9]
+Num.forEach((square)=>{
+    console.log("Square of "+ square +" is " + (square*square));   
+
+},)
