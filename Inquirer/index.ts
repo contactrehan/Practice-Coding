964a40b4874531b2c23ca95d60dc1ca242409e25
contactrehan <contactrehan2209@gmail.com>
2023-09-23 08:21:51 +0500

    -A

---
+++ Inquirer/index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+var user = await inquirer.prompt([
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your username?",
+    validate: (value) => {
+      if (!value) {
+        return "Please Enter Your userName.";
+      }
+      return true;
+    },
+  },
+
+  {
+    type: "checkbox",
+    name:"colorChoice",
+    message: "Select which platform share the same username:",
+    choices: ["GitHub", "Twitter", "Linkedin", "Instagram"],
+    default:["Github"]
+  },
+]);
+console.log(user);
