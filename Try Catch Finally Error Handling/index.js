5891f6dc3de47f8e9b8fb4e3d906fe99e3565cc9
contactrehan <contactrehan2209@gmail.com>
2023-10-21 17:47:12 +0500

    added finally

---
+++ Try Catch Finally Error Handling/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Error Handling
+console.log("Start");
+try {
+    let a = "Rehan";
+    console.log(a);
+}
+catch (error) {
+    console.log("Error", error);
+}
+console.log("End");
+console.log("the end");
---
+++ Try Catch Finally Error Handling/index.ts
@@ -0,0 +1,21 @@
+// Error Handling
+console.log("Start");
+
+try {
+    let a:string = "Rehan"
+console.log(a);
+
+    
+} catch (error) {
+    console.log("Error",error);
+    
+}
+finally{
+    console.log("Its Working");
+    
+}
+
+
+console.log("End");
+console.log("the end");
+
