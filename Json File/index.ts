0eddd73e6846a7f45ab15aa2bd56e045a6529a1e
contactrehan <contactrehan2209@gmail.com>
2023-10-22 05:21:05 +0500

    Learned JSon file importing and chalk Cli

---
+++ Json File/index.ts
@@ -0,0 +1,14 @@
+import countries from "./countries.json" assert {type:"json"}
+import chalk from "chalk"
+
+
+for (let index = 0; index < countries.length; index++) {
+    const element = countries[index];
+    // if (element.name === "P") {
+        
+    
+    console.log(`Country Name:`+ chalk.white.bgCyan.italic.bold(element.name) +`Country Code`+chalk.white.bgRed.italic.bold(element.code) )
+    // }
+    
+}
+chalk.white.bgCyan.italic.bold()
